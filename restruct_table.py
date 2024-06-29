ac569e707a2b72899358b76cb0e79c4bab0e7907
Bogdan Romanov <kaperlinkor@yandex.ru>
2024-06-30 01:26:50 +0300

    Готовые таблицы

--- restruct_table.py
+++ restruct_table.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import re
 
-df = pd.read_csv(r"Hp_Картриджи_2.csv")
+df = pd.read_csv(r"c:\Users\kaper\Desktop\35-Картриджи-Sharp-2024-06-30.csv")
 
 pattern_compatible = r'<br/><a href="#add=\d+" class="buy" rel="nofollow, noindex">Купить</a>'
 pattern_unnamed_2 = r'<br/><a href="#add=\d+&qty=\d+" class="buy" rel="nofollow, noindex">Купить</a>'
@@ -22,10 +22,21 @@ def remove_orig(value):
         return re.sub(pattern_orig, '', value)
     return value
 
-df['Cовместимый'] = df['Cовместимый'].apply(remove_compatible)
-df['Unnamed: 2'] = df['Unnamed: 2'].apply(remove_unnamed_2)
-df['Ориг.'] = df['Ориг.'].apply(remove_orig)
+def replace_color(value):
+    replacements = {
+        '<span style="background: black"> </span>': '<span class="rini_color-square-black"></span>',
+        '<span style="background: #FFD700"> </span>': '<span class="rini_color-square-yellow"></span>',
+        '<span style="background: #00BFFF"> </span>': '<span class="rini_color-square-cyan"></span>',
+        '<span style="background: darkmagenta"> </span>': '<span class="rini_color-square-magenta"></span>',
+        '<em style="background: blue"> </em><em style="background: red"> </em><em style="background: yellow"> </em>': '<span class="rini_color-square"></span>'
+    }
+    return replacements.get(value, value)
 
-df.to_csv(r'./Hp-Картриджи_cleaned.csv', index=False)
+df['Cовместимый'] = df['Cовместимый'].apply(remove_compatible)
+df['Cовместимый 10 шт.'] = df['Cовместимый 10 шт.'].apply(remove_unnamed_2)
+df['Ориг.'] = df['Ориг.'].apply(remove_orig)
+df['Цвет'] = df['Цвет'].apply(replace_color)
+
+df.to_csv(r'./sharp-cartridges_cleaned_full.csv', index=False)
 
 print(df.columns)
